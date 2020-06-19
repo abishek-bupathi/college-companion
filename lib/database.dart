89dae0bac820e3c1a8b0d85ae1f04061c4989731
Abishek Bupathi <abishekb128@gmail.com>
2020-06-19 11:28:21 +0100

    Created db and queries for activity

--- lib/database.dart
+++ lib/database.dart
@@ -12,7 +12,19 @@ class Tasks extends Table{
 
 }
 
-@UseMoor(tables: [Tasks])
+class Activity extends Table{
+
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text().withLength(min: 1)();
+  TextColumn get note => text().withDefault(Constant(""))();
+  TextColumn get location => text().withDefault(Constant(""))();
+  DateTimeColumn get date => dateTime().nullable()();
+  BoolColumn get completed => boolean().withDefault(Constant(false))();
+
+}
+
+
+@UseMoor(tables: [Tasks, Activity])
 class AppDatabase extends _$AppDatabase{
 
   AppDatabase() : super(FlutterQueryExecutor.inDatabaseFolder(path: 'db.sqlite', logStatements: true));
@@ -25,5 +37,12 @@ class AppDatabase extends _$AppDatabase{
   Future insertTask(Task task) => into(tasks).insert(task);
   Future updateTask(Task task) => update(tasks).replace(task);
   Future deleteTask(Task task) => delete(tasks).delete(task);
+
+  Future<List<ActivityData>> getAllActivities() => select(activity).get();
+  Stream<List<ActivityData>> watchAllActivities() => select(activity).watch();
+  Future insertActivity(ActivityData activity_item) => into(activity).insert(activity_item);
+  Future updateActivity(ActivityData activity_item) => update(activity).replace(activity_item);
+  Future deleteActivity(ActivityData activity_item) => delete(activity).delete(activity_item);
+
 
 }
--- lib/database.g.dart
+++ lib/database.g.dart
@@ -361,12 +361,362 @@ class $TasksTable extends Tasks with TableInfo<$TasksTable, Task> {
   }
 }
 
+class ActivityData extends DataClass implements Insertable<ActivityData> {
+  final int id;
+  final String title;
+  final String note;
+  final String location;
+  final DateTime date;
+  final bool completed;
+  ActivityData(
+      {@required this.id,
+      @required this.title,
+      @required this.note,
+      @required this.location,
+      this.date,
+      @required this.completed});
+  factory ActivityData.fromData(Map<String, dynamic> data, GeneratedDatabase db,
+      {String prefix}) {
+    final effectivePrefix = prefix ?? '';
+    final intType = db.typeSystem.forDartType<int>();
+    final stringType = db.typeSystem.forDartType<String>();
+    final dateTimeType = db.typeSystem.forDartType<DateTime>();
+    final boolType = db.typeSystem.forDartType<bool>();
+    return ActivityData(
+      id: intType.mapFromDatabaseResponse(data['${effectivePrefix}id']),
+      title:
+          stringType.mapFromDatabaseResponse(data['${effectivePrefix}title']),
+      note: stringType.mapFromDatabaseResponse(data['${effectivePrefix}note']),
+      location: stringType
+          .mapFromDatabaseResponse(data['${effectivePrefix}location']),
+      date:
+          dateTimeType.mapFromDatabaseResponse(data['${effectivePrefix}date']),
+      completed:
+          boolType.mapFromDatabaseResponse(data['${effectivePrefix}completed']),
+    );
+  }
+  @override
+  Map<String, Expression> toColumns(bool nullToAbsent) {
+    final map = <String, Expression>{};
+    if (!nullToAbsent || id != null) {
+      map['id'] = Variable<int>(id);
+    }
+    if (!nullToAbsent || title != null) {
+      map['title'] = Variable<String>(title);
+    }
+    if (!nullToAbsent || note != null) {
+      map['note'] = Variable<String>(note);
+    }
+    if (!nullToAbsent || location != null) {
+      map['location'] = Variable<String>(location);
+    }
+    if (!nullToAbsent || date != null) {
+      map['date'] = Variable<DateTime>(date);
+    }
+    if (!nullToAbsent || completed != null) {
+      map['completed'] = Variable<bool>(completed);
+    }
+    return map;
+  }
+
+  ActivityCompanion toCompanion(bool nullToAbsent) {
+    return ActivityCompanion(
+      id: id == null && nullToAbsent ? const Value.absent() : Value(id),
+      title:
+          title == null && nullToAbsent ? const Value.absent() : Value(title),
+      note: note == null && nullToAbsent ? const Value.absent() : Value(note),
+      location: location == null && nullToAbsent
+          ? const Value.absent()
+          : Value(location),
+      date: date == null && nullToAbsent ? const Value.absent() : Value(date),
+      completed: completed == null && nullToAbsent
+          ? const Value.absent()
+          : Value(completed),
+    );
+  }
+
+  factory ActivityData.fromJson(Map<String, dynamic> json,
+      {ValueSerializer serializer}) {
+    serializer ??= moorRuntimeOptions.defaultSerializer;
+    return ActivityData(
+      id: serializer.fromJson<int>(json['id']),
+      title: serializer.fromJson<String>(json['title']),
+      note: serializer.fromJson<String>(json['note']),
+      location: serializer.fromJson<String>(json['location']),
+      date: serializer.fromJson<DateTime>(json['date']),
+      completed: serializer.fromJson<bool>(json['completed']),
+    );
+  }
+  @override
+  Map<String, dynamic> toJson({ValueSerializer serializer}) {
+    serializer ??= moorRuntimeOptions.defaultSerializer;
+    return <String, dynamic>{
+      'id': serializer.toJson<int>(id),
+      'title': serializer.toJson<String>(title),
+      'note': serializer.toJson<String>(note),
+      'location': serializer.toJson<String>(location),
+      'date': serializer.toJson<DateTime>(date),
+      'completed': serializer.toJson<bool>(completed),
+    };
+  }
+
+  ActivityData copyWith(
+          {int id,
+          String title,
+          String note,
+          String location,
+          DateTime date,
+          bool completed}) =>
+      ActivityData(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        note: note ?? this.note,
+        location: location ?? this.location,
+        date: date ?? this.date,
+        completed: completed ?? this.completed,
+      );
+  @override
+  String toString() {
+    return (StringBuffer('ActivityData(')
+          ..write('id: $id, ')
+          ..write('title: $title, ')
+          ..write('note: $note, ')
+          ..write('location: $location, ')
+          ..write('date: $date, ')
+          ..write('completed: $completed')
+          ..write(')'))
+        .toString();
+  }
+
+  @override
+  int get hashCode => $mrjf($mrjc(
+      id.hashCode,
+      $mrjc(
+          title.hashCode,
+          $mrjc(
+              note.hashCode,
+              $mrjc(location.hashCode,
+                  $mrjc(date.hashCode, completed.hashCode))))));
+  @override
+  bool operator ==(dynamic other) =>
+      identical(this, other) ||
+      (other is ActivityData &&
+          other.id == this.id &&
+          other.title == this.title &&
+          other.note == this.note &&
+          other.location == this.location &&
+          other.date == this.date &&
+          other.completed == this.completed);
+}
+
+class ActivityCompanion extends UpdateCompanion<ActivityData> {
+  final Value<int> id;
+  final Value<String> title;
+  final Value<String> note;
+  final Value<String> location;
+  final Value<DateTime> date;
+  final Value<bool> completed;
+  const ActivityCompanion({
+    this.id = const Value.absent(),
+    this.title = const Value.absent(),
+    this.note = const Value.absent(),
+    this.location = const Value.absent(),
+    this.date = const Value.absent(),
+    this.completed = const Value.absent(),
+  });
+  ActivityCompanion.insert({
+    this.id = const Value.absent(),
+    @required String title,
+    this.note = const Value.absent(),
+    this.location = const Value.absent(),
+    this.date = const Value.absent(),
+    this.completed = const Value.absent(),
+  }) : title = Value(title);
+  static Insertable<ActivityData> custom({
+    Expression<int> id,
+    Expression<String> title,
+    Expression<String> note,
+    Expression<String> location,
+    Expression<DateTime> date,
+    Expression<bool> completed,
+  }) {
+    return RawValuesInsertable({
+      if (id != null) 'id': id,
+      if (title != null) 'title': title,
+      if (note != null) 'note': note,
+      if (location != null) 'location': location,
+      if (date != null) 'date': date,
+      if (completed != null) 'completed': completed,
+    });
+  }
+
+  ActivityCompanion copyWith(
+      {Value<int> id,
+      Value<String> title,
+      Value<String> note,
+      Value<String> location,
+      Value<DateTime> date,
+      Value<bool> completed}) {
+    return ActivityCompanion(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      note: note ?? this.note,
+      location: location ?? this.location,
+      date: date ?? this.date,
+      completed: completed ?? this.completed,
+    );
+  }
+
+  @override
+  Map<String, Expression> toColumns(bool nullToAbsent) {
+    final map = <String, Expression>{};
+    if (id.present) {
+      map['id'] = Variable<int>(id.value);
+    }
+    if (title.present) {
+      map['title'] = Variable<String>(title.value);
+    }
+    if (note.present) {
+      map['note'] = Variable<String>(note.value);
+    }
+    if (location.present) {
+      map['location'] = Variable<String>(location.value);
+    }
+    if (date.present) {
+      map['date'] = Variable<DateTime>(date.value);
+    }
+    if (completed.present) {
+      map['completed'] = Variable<bool>(completed.value);
+    }
+    return map;
+  }
+}
+
+class $ActivityTable extends Activity
+    with TableInfo<$ActivityTable, ActivityData> {
+  final GeneratedDatabase _db;
+  final String _alias;
+  $ActivityTable(this._db, [this._alias]);
+  final VerificationMeta _idMeta = const VerificationMeta('id');
+  GeneratedIntColumn _id;
+  @override
+  GeneratedIntColumn get id => _id ??= _constructId();
+  GeneratedIntColumn _constructId() {
+    return GeneratedIntColumn('id', $tableName, false,
+        hasAutoIncrement: true, declaredAsPrimaryKey: true);
+  }
+
+  final VerificationMeta _titleMeta = const VerificationMeta('title');
+  GeneratedTextColumn _title;
+  @override
+  GeneratedTextColumn get title => _title ??= _constructTitle();
+  GeneratedTextColumn _constructTitle() {
+    return GeneratedTextColumn('title', $tableName, false, minTextLength: 1);
+  }
+
+  final VerificationMeta _noteMeta = const VerificationMeta('note');
+  GeneratedTextColumn _note;
+  @override
+  GeneratedTextColumn get note => _note ??= _constructNote();
+  GeneratedTextColumn _constructNote() {
+    return GeneratedTextColumn('note', $tableName, false,
+        defaultValue: Constant(""));
+  }
+
+  final VerificationMeta _locationMeta = const VerificationMeta('location');
+  GeneratedTextColumn _location;
+  @override
+  GeneratedTextColumn get location => _location ??= _constructLocation();
+  GeneratedTextColumn _constructLocation() {
+    return GeneratedTextColumn('location', $tableName, false,
+        defaultValue: Constant(""));
+  }
+
+  final VerificationMeta _dateMeta = const VerificationMeta('date');
+  GeneratedDateTimeColumn _date;
+  @override
+  GeneratedDateTimeColumn get date => _date ??= _constructDate();
+  GeneratedDateTimeColumn _constructDate() {
+    return GeneratedDateTimeColumn(
+      'date',
+      $tableName,
+      true,
+    );
+  }
+
+  final VerificationMeta _completedMeta = const VerificationMeta('completed');
+  GeneratedBoolColumn _completed;
+  @override
+  GeneratedBoolColumn get completed => _completed ??= _constructCompleted();
+  GeneratedBoolColumn _constructCompleted() {
+    return GeneratedBoolColumn('completed', $tableName, false,
+        defaultValue: Constant(false));
+  }
+
+  @override
+  List<GeneratedColumn> get $columns =>
+      [id, title, note, location, date, completed];
+  @override
+  $ActivityTable get asDslTable => this;
+  @override
+  String get $tableName => _alias ?? 'activity';
+  @override
+  final String actualTableName = 'activity';
+  @override
+  VerificationContext validateIntegrity(Insertable<ActivityData> instance,
+      {bool isInserting = false}) {
+    final context = VerificationContext();
+    final data = instance.toColumns(true);
+    if (data.containsKey('id')) {
+      context.handle(_idMeta, id.isAcceptableOrUnknown(data['id'], _idMeta));
+    }
+    if (data.containsKey('title')) {
+      context.handle(
+          _titleMeta, title.isAcceptableOrUnknown(data['title'], _titleMeta));
+    } else if (isInserting) {
+      context.missing(_titleMeta);
+    }
+    if (data.containsKey('note')) {
+      context.handle(
+          _noteMeta, note.isAcceptableOrUnknown(data['note'], _noteMeta));
+    }
+    if (data.containsKey('location')) {
+      context.handle(_locationMeta,
+          location.isAcceptableOrUnknown(data['location'], _locationMeta));
+    }
+    if (data.containsKey('date')) {
+      context.handle(
+          _dateMeta, date.isAcceptableOrUnknown(data['date'], _dateMeta));
+    }
+    if (data.containsKey('completed')) {
+      context.handle(_completedMeta,
+          completed.isAcceptableOrUnknown(data['completed'], _completedMeta));
+    }
+    return context;
+  }
+
+  @override
+  Set<GeneratedColumn> get $primaryKey => {id};
+  @override
+  ActivityData map(Map<String, dynamic> data, {String tablePrefix}) {
+    final effectivePrefix = tablePrefix != null ? '$tablePrefix.' : null;
+    return ActivityData.fromData(data, _db, prefix: effectivePrefix);
+  }
+
+  @override
+  $ActivityTable createAlias(String alias) {
+    return $ActivityTable(_db, alias);
+  }
+}
+
 abstract class _$AppDatabase extends GeneratedDatabase {
   _$AppDatabase(QueryExecutor e) : super(SqlTypeSystem.defaultInstance, e);
   $TasksTable _tasks;
   $TasksTable get tasks => _tasks ??= $TasksTable(this);
+  $ActivityTable _activity;
+  $ActivityTable get activity => _activity ??= $ActivityTable(this);
   @override
   Iterable<TableInfo> get allTables => allSchemaEntities.whereType<TableInfo>();
   @override
-  List<DatabaseSchemaEntity> get allSchemaEntities => [tasks];
+  List<DatabaseSchemaEntity> get allSchemaEntities => [tasks, activity];
 }
